360c366ca5d51d7c299eedc6072dbb356d7790d2
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-10 23:12:13 -0300

    feat(atoms): cria componente AlertSnackbar

---
+++ frontend/src/components/atoms/AlertSnackbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Snackbar, Alert } from '@mui/material'
+
+export default function AlertSnackbar({ open, onClose, severity, message }) {
+    return (
+        <Snackbar open={open} autoHideDuration={4000} onClose={onClose} anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
+            <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
+                {message}
+            </Alert>
+        </Snackbar>
+    )
+}
