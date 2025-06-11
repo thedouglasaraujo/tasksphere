f458aef9816384462613ed72035b40183463908a
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-10 23:21:43 -0300

    feat(molecules): cria componente LoginForm

---
+++ frontend/src/components/molecules/LoginForm/index.js
@@ -0,0 +1,35 @@
+import { Box, Button } from '@mui/material'
+import InputField from '../../atoms/InputField'
+
+export default function LoginForm({ onSubmit, register, errors, handleSubmit, sxButton, sxLabel, sxLabelWithMarginTop }) {
+    return (
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate>
+            <InputField
+                label="Email"
+                name="email"
+                register={register}
+                rules={{
+                    required: 'Email é obrigatório',
+                    pattern: { value: /^\S+@\S+$/i, message: 'Email inválido' },
+                }}
+                errors={errors}
+                sxLabel={sxLabel}
+            />
+            <InputField
+                label="Senha"
+                name="password"
+                type="password"
+                register={register}
+                rules={{
+                    required: 'Senha é obrigatória',
+                    minLength: { value: 6, message: 'Mínimo de 6 caracteres' },
+                }}
+                errors={errors}
+                sxLabel={sxLabelWithMarginTop}
+            />
+            <Button type="submit" fullWidth variant="contained" sx={sxButton}>
+                Entrar
+            </Button>
+        </Box>
+    )
+}
