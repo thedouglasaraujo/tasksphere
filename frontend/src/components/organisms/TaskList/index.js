65a4328b1904501a895f185683ff4bd4f0be4bc6
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-14 15:06:14 -0300

    feat(organisms): exibe feedback quando não há tasks

--- frontend/src/components/organisms/TaskList/index.js
+++ frontend/src/components/organisms/TaskList/index.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { List } from '@mui/material'
-import TaskListItem from '../../molecules/TaskListItem'
+import React from 'react';
+import { List, Typography } from '@mui/material';
+import TaskListItem from '../../molecules/TaskListItem';
 
-export default function TaskList({ tasks }) {
+export default function TaskList({ tasks = [] }) {  
     return (
         <List>
-            {tasks.map(task => (
-                <TaskListItem key={task.id} task={task} />
-            ))}
+            {tasks.length === 0 ? (
+                <Typography variant="body2" color="textSecondary">
+                    Nenhuma tarefa encontrada.
+                </Typography>
+            ) : (
+                tasks.map((task) => (
+                    <TaskListItem key={task.id} task={task} />
+                ))
+            )}
         </List>
-    )
+    );
 }
