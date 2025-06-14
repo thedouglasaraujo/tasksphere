d6a0b0afed60cd382126b2a994d9136c9c155d43
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-13 22:00:06 -0300

    feat(models): adiciona arquivo base dos models

---
+++ backend/src/models/index.js
@@ -0,0 +1,39 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const basename = path.basename(__filename);
+const db = {};
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false
+    }
+);
+
+fs
+    .readdirSync(__dirname)
+    .filter(file => file !== basename && file.endsWith('.js'))
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
