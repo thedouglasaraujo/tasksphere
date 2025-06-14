603256a632d22e5143a05003241b728252d9b82a
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-14 14:05:23 -0300

    feat(atoms): cria componente LoadingIndicator

---
+++ frontend/src/components/atoms/LoadingIndicator/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box, CircularProgress, Typography } from '@mui/material';
+
+export default function LoadingIndicator({ message = 'Carregando...' }) {
+    return (
+        <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={4}>
+            <CircularProgress />
+            <Typography mt={2} color="textSecondary">
+                {message}
+            </Typography>
+        </Box>
+    );
+}
