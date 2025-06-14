a7173a2d881ee2ed90fa380a6035fe6cc2546c3c
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-13 23:13:54 -0300

    feat(migrations): cria migration da entidade Project

---
+++ backend/src/migrations/20250614011915-create-project.js
@@ -0,0 +1,51 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING(500)
+      },
+      start_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      end_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+      },
+      creator_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('NOW')
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Projects');
+  }
+};
