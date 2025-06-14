c9fc19c6932ae2ab2f316737138dcf42f2f297f6
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-14 13:49:30 -0300

    feat(atoms): cria componente ErrorMessage

---
+++ frontend/src/components/atoms/ErrorMessage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+export default function ErrorMessage({ message = 'Algo deu errado.' }) {
+    return (
+        <Box display="flex" justifyContent="center" alignItems="center" mt={4}>
+            <Typography color="error">{message}</Typography>
+        </Box>
+    );
+}
