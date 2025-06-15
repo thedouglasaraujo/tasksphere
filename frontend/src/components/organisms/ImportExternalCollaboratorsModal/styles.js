e1799f6848bec824107ed20a60b41ea32052fd1d
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-14 22:08:39 -0300

    feat(organisms): cria componente ImportExternalCollaboratorsModal

---
+++ frontend/src/components/organisms/ImportExternalCollaboratorsModal/styles.js
@@ -0,0 +1,20 @@
+const styles = {
+    modalBox: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+        p: 4,
+        borderRadius: 2,
+        width: 400,
+        maxWidth: '90%',
+    },
+    title: {
+        fontWeight: 600,
+        color: '#2C3E50',
+    },
+}
+
+export default styles
