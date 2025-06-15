c50b936fc2fd9ecaa423556854d0e2e1251d04ee
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-15 13:15:53 -0300

    refactor(atoms): ajusta estilização do LoadingIndicator

--- frontend/src/components/atoms/LoadingIndicator/index.js
+++ frontend/src/components/atoms/LoadingIndicator/index.js
@@ -2,9 +2,16 @@ import { Box, CircularProgress, Typography } from '@mui/material';
 
 export default function LoadingIndicator({ message = 'Carregando...' }) {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={4}>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      height="90vh"
+      width="100vw"
+    >
       <CircularProgress />
-      <Typography mt={2} color="textSecondary">
+      <Typography mt={2} color="primary">
         {message}
       </Typography>
     </Box>
