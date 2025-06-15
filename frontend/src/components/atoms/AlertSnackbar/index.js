82ed7c589cfcafab42a5e47c3c15f7e44c50afee
thedouglasaraujo <thedouglasaraujo@gmail.com>
2025-06-15 11:16:51 -0300

    refactor(atoms): altera cores do AlertSnackbar

--- frontend/src/components/atoms/AlertSnackbar/index.js
+++ frontend/src/components/atoms/AlertSnackbar/index.js
@@ -1,6 +1,11 @@
-import { Alert, Snackbar } from '@mui/material';
+import { Alert, Snackbar } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import stylesFn from './styles'
 
 export default function AlertSnackbar({ open, onClose, severity, message }) {
+  const theme = useTheme()
+  const styles = stylesFn(theme)
+
   return (
     <Snackbar
       open={open}
@@ -8,9 +13,13 @@ export default function AlertSnackbar({ open, onClose, severity, message }) {
       onClose={onClose}
       anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
     >
-      <Alert onClose={onClose} severity={severity} sx={{ width: '100%' }}>
+      <Alert
+        onClose={onClose}
+        severity={severity}
+        sx={styles.alert(severity)}
+      >
         {message}
       </Alert>
     </Snackbar>
-  );
+  )
 }
